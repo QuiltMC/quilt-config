b8e167e98ac39574bbbc86647247c42e2dce3f99
Glitch <glitch.g3431@gmail.com>
2023-04-23 12:56:53 -0500

    Licenses

--- src/main/java/org/quiltmc/config/api/naming/NamingSchemes.java
+++ src/main/java/org/quiltmc/config/api/naming/NamingSchemes.java
@@ -1,12 +1,12 @@
 /*
- * Configurate
- * Copyright (C) zml and Configurate contributors
+ * Copyright zml and Configurate contributors
+ * Copyright 2023 QuiltMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,11 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.quiltmc.config.api.naming;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 /**
  * Standard naming schemes.
  */
